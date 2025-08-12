bdc29b4e2ee386eca1a4b7b8d9baf9cc5588273a
Darshan-gowda-m <155784473+Darshan-gowda-m@users.noreply.github.com>
2025-08-12 22:24:17 +0530

    Create 1.c

---
+++ 1/1.c
@@ -0,0 +1,22 @@
+const int red=8;
+const int yellow=9;
+const int green=10;
+void setup(){
+  pinMode(red,OUTPUT);
+   pinMode(yellow,OUTPUT);
+   pinMode(green,OUTPUT);
+}
+void light(ibt r,int y,int g,int d)
+{
+  digitalWrite(red,r);
+  digitalWrite(yellow,y);
+  digitalWrite(green,g);
+  delay(d);
+}
+void loop()
+{
+  light(HIGH,LOW,LOW,1000);
+  light(LOW,HIGH,LOW,1000);
+  light(LOW,LOW,HIGH,1000);
+  delay(1000);
+}
